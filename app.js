d3905a8349c7bd3d5cede6f14e0d76fedf479ab1
m00nbek <melikulov990@gmail.com>
2023-01-05 10:58:39 +0500

    whole fuckin app

---
+++ app.js
@@ -0,0 +1,94 @@
+import 'dotenv/config';
+import express from 'express';
+import {
+  InteractionType,
+  InteractionResponseType,
+  InteractionResponseFlags,
+  MessageComponentTypes,
+  ButtonStyleTypes,
+} from 'discord-interactions';
+import { VerifyDiscordRequest, DiscordRequest } from './utils.js';
+import { getResult } from './game.js';
+import {
+  CHALLENGE_COMMAND,
+  OHAYO_COMMAND,
+  HasGuildCommands,
+} from './commands.js';
+
+// Create an express app
+const app = express();
+// Get port, or default to 6969 
+const PORT = process.env.PORT || 6969;
+// Parse request body and verifies incoming requests using discord-interactions package
+app.use(express.json({ verify: VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
+
+// Store for in-progress games. In production, you'd want to use a DB
+const activeGames = {};
+
+/**
+ * Interactions endpoint URL where Discord will send HTTP requests
+ */
+app.post('/interactions', async function (req, res) {
+  // Interaction type and data
+  const { type, id, data } = req.body;
+
+  /**
+   * Handle verification requests
+   */
+  if (type === InteractionType.PING) {
+    return res.send({ type: InteractionResponseType.PONG });
+  }
+
+  /**
+   * Handle slash command requests
+   * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
+   */
+  if (type === InteractionType.APPLICATION_COMMAND) {
+    const { name } = data;
+
+    // "ohayo" guild command
+    if (name === 'ohayo') {
+      // Send a message into the channel where command was triggered from
+      return res.send({
+        type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+        data: {
+          content: 'Ohayo!',
+        },
+      });
+    }
+    // "challenge" guild command
+    if (name === 'challenge' && id) {
+      const userId = req.body.member.user.id;
+      // User's object choice
+      const objectName = req.body.data.options[0].value;
+
+      const game = {
+        id: userId,
+        objectName,
+      };
+        // Calculate result from helper function
+        const resultStr = getResult(game);
+
+        // Update message with token in request body
+        try {
+          // Send results
+          await res.send({
+            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            data: { content: resultStr },
+          });
+        } catch (err) {
+          console.error('Error sending message:', err);
+        }
+    }
+  }
+});
+
+app.listen(PORT, () => {
+  console.log('Listening on port', PORT);
+
+  // Check if guild commands from commands.js are installed (if not, install them)
+  HasGuildCommands(process.env.APP_ID, process.env.GUILD_ID, [
+    OHAYO_COMMAND,
+    CHALLENGE_COMMAND,
+  ]);
+});
---
+++ game.js
@@ -0,0 +1,92 @@
+import { capitalize } from './utils.js';
+import 'dotenv/config';
+
+export function getResult(p1) {
+  let p2 = {
+    id: `${process.env.APP_ID}`,
+    objectName: getRPSChoices()[Math.floor(Math.random()*getRPSChoices().length)],
+  }
+
+  let gameResult;
+
+  if (RPSChoices[p1.objectName] && RPSChoices[p1.objectName][p2.objectName]) {
+    // o1 wins
+    gameResult = {
+      win: p1,
+      lose: p2,
+      verb: RPSChoices[p1.objectName][p2.objectName],
+    };
+  } else if (
+    RPSChoices[p2.objectName] &&
+    RPSChoices[p2.objectName][p1.objectName]
+  ) {
+    // o2 wins
+    gameResult = {
+      win: p2,
+      lose: p1,
+      verb: RPSChoices[p2.objectName][p1.objectName],
+    };
+  } else {
+    // tie -- win/lose don't
+    gameResult = { win: p1, lose: p2, verb: 'tie' };
+  }
+
+  return formatResult(gameResult);
+}
+
+function formatResult(result) {
+  const { win, lose, verb } = result;
+  return verb === 'tie'
+    ? `<@${win.id}> and <@${lose.id}> draw with **${win.objectName}**`
+    : `<@${win.id}>'s **${win.objectName}** ${verb} <@${lose.id}>'s **${lose.objectName}**`;
+}
+
+// this is just to figure out winner + verb
+const RPSChoices = {
+  rock: {
+    description: 'sedimentary, igneous, or perhaps even metamorphic',
+    virus: 'outwaits',
+    computer: 'smashes',
+    scissors: 'crushes',
+  },
+  cowboy: {
+    description: 'yeehaw~',
+    scissors: 'puts away',
+    wumpus: 'lassos',
+    rock: 'steel-toe kicks',
+  },
+  scissors: {
+    description: 'careful ! sharp ! edges !!',
+    paper: 'cuts',
+    computer: 'cuts cord of',
+    virus: 'cuts DNA of',
+  },
+  virus: {
+    description: 'genetic mutation, malware, or something inbetween',
+    cowboy: 'infects',
+    computer: 'corrupts',
+    wumpus: 'infects',
+  },
+  computer: {
+    description: 'beep boop beep bzzrrhggggg',
+    cowboy: 'overwhelms',
+    paper: 'uninstalls firmware for',
+    wumpus: 'deletes assets for',
+  },
+  wumpus: {
+    description: 'the purple Discord fella',
+    paper: 'draws picture on',
+    rock: 'paints cute face on',
+    scissors: 'admires own reflection in',
+  },
+  paper: {
+    description: 'versatile and iconic',
+    virus: 'ignores',
+    cowboy: 'gives papercut to',
+    rock: 'covers',
+  },
+};
+
+export function getRPSChoices() {
+  return Object.keys(RPSChoices);
+}
